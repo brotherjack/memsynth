3a73423638420031e5e06e29768ba0b3da07bbd6
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-04-07 15:17:46 -0400

    First of the load tests pass

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,7 +1,15 @@
-import pytest
+import os
+
 import pandas as pd
+
+from memsynth import config
 
 
 def test_load_excel_file():
-    df = pd.read_excel("fakeodsa.xlsx")
+    """Checks to make sure that the membership file can be loaded
+
+    This test requires the "fakeodsa.xlsx" file with an `AK_ID` column
+    """
+    path = os.path.join(config.TEST_DIR, "fakeodsa.xlsx")
+    df = pd.read_excel(path)
     assert "AK_ID" in df.columns
