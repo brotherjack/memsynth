9c751c389c96da316048e0a176a8b4c2195a8c96
Thomas Adriaan Hellinger <brotherjack1877@gmail.com>
2019-07-01 14:43:27 -0400

    Failing test to return False on a list with only soft errors

--- tests/test_memsynther.py
+++ tests/test_memsynther.py
@@ -33,3 +33,7 @@ def test_memsynther_loads_datetimes_correctly(memsynther):
 @pytest.mark.usefixtures("memsynther_ideallist")
 def test_verify_memlist_data_integrity_on_ideal_list(memsynther_ideallist):
     assert memsynther_ideallist.verify_memlist_data_integrity() == True
+
+@pytest.mark.usefixtures("memsynther_ideallist")
+def test_verify_memlist_data_integrity_on_less_than_ideal_list(memsynther):
+    assert memsynther.verify_memlist_data_integrity() == False
